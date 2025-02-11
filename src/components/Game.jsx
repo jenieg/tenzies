694d08cfc763c6376063e70e9bb584bc3d60c690
Jennifer <jmarthagodfrey@gmail.com>
2025-02-11 12:10:07 -0700

    intial files to make tenzies

---
+++ src/components/Game.jsx
@@ -0,0 +1,10 @@
+const Game = () => {
+    return (
+        <main className="h-screen w-screen flex items-center justify-center bg-gray-900">
+            <div className="h-[90vh] w-[90vw] rounded-xl bg-gray-100">
+            </div>
+        </main>
+    );
+};
+
+export default Game;
