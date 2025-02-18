c9de64bb9343275731bc665fd8c810bcf1471b13
Jennifer <jmarthagodfrey@gmail.com>
2025-02-18 10:02:05 -0700

    Feat: created header component, restyled components

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,31 +2,22 @@ import { useState } from "react";
 import Dice from "./Dice";
 
 const Game = () => {
+    const [dice, SetDice] = useState(rollNewDice);
 
-    const [dice, SetDice] = useState(rollNewDice)
-
-    function rollNewDice(){
+    function rollNewDice() {
         return Array(10)
             .fill()
             .map(() => Math.ceil(Math.random() * 6));
     }
 
     return (
-        <main className="h-screen w-screen flex items-center justify-center bg-gray-900">
-            <div className="h-[90vh] w-[90vw] max-w-screen-sm rounded-xl bg-gray-100">
-
-                <div className="flex justify-center mt-6">
-                    <section className="grid grid-cols-5 gap-4 w-9/12 max-w-screen-sm">
-                        {dice.map((value, index)=> (
-                            <Dice value={value} key={index} />
-                        ))}
-                        
-                    </section>
-                </div>
-                
-
-            </div>
-        </main>
+        <div className="flex justify-center">
+            <section className="grid grid-cols-5 gap-6 max-w-screen-sm">
+                {dice.map((value, index) => (
+                    <Dice value={value} key={index} />
+                ))}
+            </section>
+        </div>
     );
 };
 
